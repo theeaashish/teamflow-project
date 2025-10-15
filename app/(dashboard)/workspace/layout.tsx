32a3c1fcfb29956c62a2a959528240c2ccc781e8
Aashish Joshi <theeaashish@gmail.com>
2025-10-15 14:20:53 +0000

    feat: add create workspace and kind management api

--- app/(dashboard)/workspace/layout.tsx
+++ app/(dashboard)/workspace/layout.tsx
@@ -1,16 +1,24 @@
 import { CreateWorkspace } from "./_components/CreateWorkspace";
 import { UserNav } from "./_components/UserNav";
 import { WorkspaceList } from "./_components/WorkspaceList";
+import { orpc } from "@/lib/orpc";
+import { getQueryClient, HydrateClient } from "@/lib/query/hydration";
 
-export default function WorkspaceLayout({
+export default async function WorkspaceLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(orpc.workspace.list.queryOptions());
+
   return (
     <div className="w-full h-screen flex">
       <div className="flex flex-col items-center h-full w-16 bg-secondary px-2 py-3 border-r border-border">
-        <WorkspaceList />
+        <HydrateClient client={queryClient}>
+          <WorkspaceList />
+        </HydrateClient>
 
         <div className="mt-4">
           <CreateWorkspace />
