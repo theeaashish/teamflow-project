9f4373b5d384b64cabd343eb94929c516321d6fb
Aashish Joshi <theeaashish@gmail.com>
2025-10-19 16:20:23 +0000

    feat: add redirection to channel id

--- app/(dashboard)/workspace/[workspaceId]/page.tsx
+++ app/(dashboard)/workspace/[workspaceId]/page.tsx
@@ -1,7 +1,23 @@
-export default function WorkspaceIdPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+import { client } from '@/lib/orpc';
+import { redirect } from 'next/navigation';
+
+interface WorkspaceIdPageProps {
+  params: Promise<{ workspaceId: string }>;
+}
+
+export default async function WorkspaceIdPage({
+  params,
+}: WorkspaceIdPageProps) {
+  const { workspaceId } = await params;
+  const { channels } = await client.channel.list();
+
+  if (channels.length > 0) {
+    return redirect(`/workspace/${workspaceId}/channel/${channels[0].id}`);
+  }
+
+  return (
+    <div>
+      <h1>hey</h1>
+    </div>
+  );
 }
