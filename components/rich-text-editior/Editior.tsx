4833b89a884868dc6549fccd209f3c082264adec
Aashish Joshi <theeaashish@gmail.com>
2025-10-19 11:48:01 +0000

    feat: add all Editor buttons and add css for code block

--- components/rich-text-editior/Editior.tsx
+++ components/rich-text-editior/Editior.tsx
@@ -11,14 +11,14 @@ export function RichTextEditior() {
     editorProps: {
       attributes: {
         class:
-          'max-w-none min-h-[125px] focus:outline-none p-4 !w-full !max-w-none',
+          'max-w-none min-h-[125px] focus:outline-none p-4 !w-full !max-w-none prose dark:prose-invert marker:text-primary',
       },
     },
   });
 
   return (
     <div className="relative w-full border border-input rounded-lg overflow-hidden dark:bg-input/30 flex flex-col">
-      <MenuBar editor={editior}/>
+      <MenuBar editor={editior} />
       <EditorContent
         editor={editior}
         className="max-h-[200px] overflow-y-auto"
--- components/rich-text-editior/MenuBar.tsx
+++ components/rich-text-editior/MenuBar.tsx
@@ -1,4 +1,6 @@
-import { Editor } from '@tiptap/react';
+'use client';
+
+import { Editor, useEditorState } from '@tiptap/react';
 import {
   Tooltip,
   TooltipContent,
@@ -6,14 +8,42 @@ import {
   TooltipTrigger,
 } from '../ui/tooltip';
 import { Toggle } from '../ui/toggle';
-import { Bold } from 'lucide-react';
+import {
+  Bold,
+  Code,
+  Italic,
+  ListIcon,
+  ListOrdered,
+  Redo,
+  Strikethrough,
+  Undo,
+} from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { Button } from '../ui/button';
 
 interface MenuBarProps {
   editor: Editor | null;
 }
 
 export function MenuBar({ editor }: MenuBarProps) {
+  const editorState = useEditorState({
+    editor,
+    selector: ({ editor }) => {
+      if (!editor) return null;
+
+      return {
+        isBold: editor.isActive('bold'),
+        isItalic: editor.isActive('italic'),
+        isStrike: editor.isActive('strike'),
+        isCode: editor.isActive('code'),
+        isBulletList: editor.isActive('bulletList'),
+        isOrderedList: editor.isActive('orderedList'),
+        isUndo: editor.can().undo(),
+        isRedo: editor.can().redo(),
+      };
+    },
+  });
+
   if (!editor) {
     return null;
   }
@@ -31,13 +61,139 @@ export function MenuBar({ editor }: MenuBarProps) {
                   editor.chain().focus().toggleBold().run()
                 }
                 className={cn(
-                  editor.isActive('bold') && 'bg-muted text-muted-foreground'
+                  editorState?.isBold && 'bg-muted text-muted-foreground'
                 )}
               >
                 <Bold />
               </Toggle>
             </TooltipTrigger>
             <TooltipContent>Bold</TooltipContent>
+          </Tooltip>
+          {/* italic */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive('italic')}
+                onPressedChange={() =>
+                  editor.chain().focus().toggleItalic().run()
+                }
+                className={cn(
+                  editorState?.isItalic && 'bg-muted text-muted-foreground'
+                )}
+              >
+                <Italic />
+              </Toggle>
+            </TooltipTrigger>
+            <TooltipContent>Italic</TooltipContent>
+          </Tooltip>
+          {/* strike */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive('strike')}
+                onPressedChange={() =>
+                  editor.chain().focus().toggleStrike().run()
+                }
+                className={cn(
+                  editorState?.isStrike && 'bg-muted text-muted-foreground'
+                )}
+              >
+                <Strikethrough />
+              </Toggle>
+            </TooltipTrigger>
+            <TooltipContent>Strike</TooltipContent>
+          </Tooltip>
+          {/* strike */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive('codeBlock')}
+                onPressedChange={() =>
+                  editor.chain().focus().toggleCodeBlock().run()
+                }
+                className={cn(
+                  editorState?.isCode && 'bg-muted text-muted-foreground'
+                )}
+              >
+                <Code />
+              </Toggle>
+            </TooltipTrigger>
+            <TooltipContent>Code Block</TooltipContent>
+          </Tooltip>
+        </div>
+        <div className="w-px h-6 bg-border mx-2" />
+        <div className="flex flex-wrap gap-1">
+          {/* bullet list */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive('bulletList')}
+                onPressedChange={() =>
+                  editor.chain().focus().toggleBulletList().run()
+                }
+                className={cn(
+                  editorState?.isBulletList && 'bg-muted text-muted-foreground'
+                )}
+              >
+                <ListIcon />
+              </Toggle>
+            </TooltipTrigger>
+            <TooltipContent>Bullet List</TooltipContent>
+          </Tooltip>
+          {/* bullet list */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive('orderedList')}
+                onPressedChange={() =>
+                  editor.chain().focus().toggleOrderedList().run()
+                }
+                className={cn(
+                  editorState?.isOrderedList && 'bg-muted text-muted-foreground'
+                )}
+              >
+                <ListOrdered />
+              </Toggle>
+            </TooltipTrigger>
+            <TooltipContent>Ordered List</TooltipContent>
+          </Tooltip>
+        </div>
+        <div className="w-px h-6 bg-border mx-2" />
+        <div className="flex flex-wrap gap-1">
+          {/* undo */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                onClick={() => editor.chain().focus().undo().run()}
+                disabled={!editorState?.isUndo}
+                size={'sm'}
+                variant={'ghost'}
+                type="button"
+              >
+                <Undo />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Undo</TooltipContent>
+          </Tooltip>
+          {/* redo */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                onClick={() => editor.chain().focus().redo().run()}
+                disabled={!editorState?.isRedo}
+                size={'sm'}
+                variant={'ghost'}
+                type="button"
+              >
+                <Redo />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Redo</TooltipContent>
           </Tooltip>
         </div>
       </TooltipProvider>
