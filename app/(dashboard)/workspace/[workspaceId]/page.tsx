86c43d2a84a92f768d28d3eeac1d4822d6f29b4e
Aashish Joshi <theeaashish@gmail.com>
2025-10-20 07:12:11 +0000

    feat: add the empty shadcn component in workspace page

--- app/(dashboard)/workspace/[workspaceId]/page.tsx
+++ app/(dashboard)/workspace/[workspaceId]/page.tsx
@@ -1,5 +1,16 @@
+import { Button } from '@/components/ui/button';
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty';
 import { client } from '@/lib/orpc';
+import { AlertCircle } from 'lucide-react';
 import { redirect } from 'next/navigation';
+import { CreateNewChannel } from './_components/CreateNewChannel';
 
 interface WorkspaceIdPageProps {
   params: Promise<{ workspaceId: string }>;
@@ -9,15 +20,40 @@ export default async function WorkspaceIdPage({
   params,
 }: WorkspaceIdPageProps) {
   const { workspaceId } = await params;
-  const { channels } = await client.channel.list();
+  // const { channels } = await client.channel.list();
+  let channels;
+
+  try {
+    const result = await client.channel.list();
+    channels = result.channels;
+  } catch (error) {
+    console.error('Failed to fetch channels:', error);
+    return (
+      <div>
+        <h1>Error loading channels</h1>
+        <p>Please try again later.</p>
+      </div>
+    );
+  }
 
   if (channels.length > 0) {
     return redirect(`/workspace/${workspaceId}/channel/${channels[0].id}`);
   }
 
   return (
-    <div>
-      <h1>hey</h1>
-    </div>
+    <Empty className="border border-dashed">
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <AlertCircle />
+        </EmptyMedia>
+        <EmptyTitle>No channels yet!</EmptyTitle>
+        <EmptyDescription>
+          Create your first channel to get started!
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent className='mx-w-xs mx-auto'>
+        <CreateNewChannel />
+      </EmptyContent>
+    </Empty>
   );
 }
