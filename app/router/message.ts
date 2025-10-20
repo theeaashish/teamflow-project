1c332148fcc5e6c378be17c8521797a670441738
Aashish Joshi <theeaashish@gmail.com>
2025-10-20 08:48:45 +0000

    feat: add message list get request

--- app/router/message.ts
+++ app/router/message.ts
@@ -8,6 +8,7 @@ import prisma from '@/lib/prisma';
 import { createMessageSchema } from '../schemas/message';
 import { getAvatar } from '@/lib/get-avatar';
 import { Message } from '@/lib/generated/prisma';
+import { readSecurityMiddleware } from '../middlewares/arcjet/read';
 
 export const createMessage = base
   .use(requiredAuthMiddleware)
@@ -23,7 +24,6 @@ export const createMessage = base
   .input(createMessageSchema)
   .output(z.custom<Message>())
   .handler(async ({ input, context, errors }) => {
-    
     // verify channel belongs to the users organization
     const channel = await prisma.channel.findFirst({
       where: {
@@ -51,4 +51,43 @@ export const createMessage = base
     return {
       ...created,
     };
+  });
+
+export const listMessage = base
+  .use(requiredAuthMiddleware)
+  .use(requiredWorkspaceMiddleware)
+  .use(standardSecurityMiddleware)
+  .use(readSecurityMiddleware)
+  .route({
+    method: 'GET',
+    path: '/messages',
+    summary: 'List all messages',
+    tags: ['Messages'],
+  })
+  .input(z.object({
+    channelId: z.string(),
+  }))
+  .output(z.array(z.custom<Message>()))
+  .handler(async ({ input, context, errors }) => {
+    const channel = await prisma.channel.findFirst({
+      where: {
+        id: input.channelId,
+        workspaceId: context.workspace.orgCode,
+      },
+    });
+
+    if (!channel) {
+      throw errors.FORBIDDEN();
+    }
+
+    const data = await prisma.message.findMany({
+      where: {
+        channelId: input.channelId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return data;
   });
