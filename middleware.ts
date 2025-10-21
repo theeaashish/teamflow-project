cfa6fc2b97d100edea628f629940f7ce45628184
Aashish Joshi <theeaashish@gmail.com>
2025-10-21 15:27:35 +0000

    feat: optimized the next middleware using withAuth helper to check if the user is authenticated or not

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import arcjet, { createMiddleware, detectBot } from '@arcjet/next';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { withAuth } from '@kinde-oss/kinde-auth-nextjs/server';
+import { NextMiddleware, NextRequest, NextResponse } from 'next/server';
 
 const aj = arcjet({
   key: process.env.ARCJET_KEY!,
@@ -18,16 +18,22 @@ const aj = arcjet({
 });
 
 async function existingMiddleware(req: NextRequest) {
-  const { getClaim } = getKindeServerSession();
-  const orgCode = await getClaim('org_code');
-
+  const anyReq = req as {
+    nextUrl: NextRequest['nextUrl'];
+    kindeAuth?: { token?: any; user?: any };
+  };
   const url = req.nextUrl;
+
+  const orgCode =
+    anyReq.kindeAuth?.user?.org_code ||
+    anyReq.kindeAuth?.token?.org_code ||
+    anyReq.kindeAuth?.token?.claims?.org_code;
 
   if (
     url.pathname.startsWith('/workspace') &&
-    !url.pathname.includes(orgCode?.value || '')
+    !url.pathname.includes(orgCode || '')
   ) {
-    url.pathname = `/workspace/${orgCode?.value}`;
+    url.pathname = `/workspace/${orgCode}`;
 
     return NextResponse.redirect(url);
   }
@@ -35,7 +41,12 @@ async function existingMiddleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-export default createMiddleware(aj, existingMiddleware);
+export default createMiddleware(
+  aj,
+  withAuth(existingMiddleware, {
+    publicPaths: ['/'],
+  }) as NextMiddleware
+);
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|/rpc).*)'],
