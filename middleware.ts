162c857761499fb218f092877eb1f42f6ecaf187
Aashish Joshi <theeaashish@gmail.com>
2025-10-22 02:54:56 +0000

    feat: add uploadthing and created a custom hook

--- middleware.ts
+++ middleware.ts
@@ -44,7 +44,7 @@ async function existingMiddleware(req: NextRequest) {
 export default createMiddleware(
   aj,
   withAuth(existingMiddleware, {
-    publicPaths: ['/'],
+    publicPaths: ['/', '/api/uploadthing'],
   }) as NextMiddleware
 );
 
