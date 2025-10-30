db8a9f231e6f7363d9bac1c6eed5975e48cdeee7
Aashish Joshi <theeaashish@gmail.com>
2025-10-30 07:38:37 +0000

    feat: add channel name

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/channelHeader.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/channelHeader.tsx
@@ -1,9 +1,13 @@
 import { ThemeToggle } from '@/components/ui/theme-toggle';
 
-export function ChannelHeader() {
+interface ChannelHeaderProps {
+  channelName: string;
+}
+
+export function ChannelHeader({ channelName }: ChannelHeaderProps) {
   return (
     <div className="flex items-center justify-between h-14 px-4 border-b">
-      <h1 className="text-lg font-semibold"># super-cool-channel</h1>
+      <h1 className="text-lg font-semibold">#{channelName}</h1>
 
       <div className="flex items-center space-x-2">
         <ThemeToggle />
--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -26,7 +26,7 @@ export default function ChannelPageMain() {
     <div className="flex h-screen w-full">
       {/* main channel are*/}
       <div className="flex flex-col flex-1 min-w-0">
-        <ChannelHeader />
+        <ChannelHeader channelName={data?.channelName as string} />
 
         {/* scrollable messages area */}
         <div className="flex-1 overflow-hidden mb-4">
